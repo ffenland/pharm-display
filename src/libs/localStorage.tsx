97e76449ea3e24ce87083537f8f244a1d8013b72
ffenland <ffenland@gmail.com>
2023-12-13 10:29:47 +0900

    prepare for deploy

--- src/libs/localStorage.tsx
+++
@@ -1,8 +0,0 @@
-export const getPlayStateFromLocal = () => {
-  const state = localStorage.getItem("play");
-  return state === "true";
-};
-
-export const setPlayStateToLocal = (state: boolean) => {
-  localStorage.setItem("play", state.toString());
-};
--- src/libs/useAuthContext.tsx
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import { AuthStateContext } from "../context/AuthContext";
-
-export const useAuthContext = () => {
-  const authState = useContext(AuthStateContext);
-  if (!authState) throw new Error("AuthProvider not found");
-  return authState;
-};
