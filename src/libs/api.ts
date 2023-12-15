f55a7c24a81efd842dad403883a092518fa7c7df
ffenland <ffenland@gmail.com>
2023-12-15 15:52:47 +0900

    d

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -29,3 +29,28 @@ export const videoTest = async ({ file }: { file: File }) => {
       .then((response) => response.data);
   }
 };
+
+interface IMeResponse {
+  ok: boolean;
+  user: {
+    uid: string;
+    key_code: string;
+  };
+}
+export const me = async (): Promise<IMeResponse | undefined> => {
+  const idToken = await auth.currentUser?.getIdToken();
+
+  if (!idToken) {
+    return undefined;
+  } else {
+    return instance
+      .get("users/me/", {
+        headers: {
+          "X-CSRFToken": Cookie.get("csrftoken") || "",
+          Authorization: `Bearer ${idToken}`, // Firebase 토큰을 Bearer 토큰으로 전달
+          // "Content-Type": "application/json", // 필요한 경우 Content-Type 설정
+        },
+      })
+      .then((response) => response.data);
+  }
+};
